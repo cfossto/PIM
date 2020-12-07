0326015f4b9d421ae80c932754b324e8679965df
Joakim <jmalmberg.web@gmail.com>
2020-12-07 15:47:09 +0100

    Added function that displays notes from array in js

--- src/frontend/www/js/script.js
+++ src/frontend/www/js/script.js
@@ -1,6 +1,43 @@
-console.log("hi");
-if (typeof jQuery == "undefined") {
-    alert("JQuery is not installed");
-} else {
-    alert("JQuery is installed correctly!");
+let notes = [
+    {
+        title: "Anteckning Titel 1",
+        text: "Phasellus gravida semper nisi. Aenean ut eros et nisl sagittis vestibulum. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris.\n" +
+        "\n" +
+        "Mauris sollicitudin fermentum libero. Etiam rhoncus. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor.\n" +
+        "\n" +
+        "Phasellus blandit leo ut odio. Nullam sagittis.."
+    },
+    {
+        title: "Anteckning Titel 2",
+        text: "Phasellus gravida semper nisi. Aenean ut eros et nisl sagittis vestibulum. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris.\n" +
+        "\n" +
+        "Mauris sollicitudin fermentum libero. Etiam rhoncus. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor.\n" +
+        "\n" +
+        "Phasellus blandit leo ut odio. Nullam sagittis.."
+    },
+    {
+        title: "Anteckning Titel 2",
+        text: "Phasellus gravida semper nisi. Aenean ut eros et nisl sagittis vestibulum. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris.\n" +
+        "\n" +
+        "Mauris sollicitudin fermentum libero. Etiam rhoncus. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor.\n" +
+        "\n" +
+        "Phasellus blandit leo ut odio. Nullam sagittis.."
+    }
+];
+
+
+function displayNotes() {
+    let allNotes = $("#all-notes");
+    allNotes.empty();
+
+    for (let note of notes) {
+        allNotes.append(`
+            <article class="note">
+                <h2>${note.title}</h2>
+                <p>${note.text}</p>
+            </article>
+        `);
+    }
 }
+
+displayNotes();
