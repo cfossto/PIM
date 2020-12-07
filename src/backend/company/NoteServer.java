6fd42cd47d4f9052a928c01541cf50579c97f438
Martina <a.martina.eriksson@hotmail.com>
2020-12-07 14:22:41 +0100

    Added server

--- src/backend/company/NoteServer.java
+++ src/backend/company/NoteServer.java
@@ -1,4 +1,24 @@
 package backend.company;
 
+import backend.company.database.Database;
+import backend.company.notePack.Note;
+import express.Express;
+
+import java.util.List;
+
 public class NoteServer {
+
+    public NoteServer() {
+
+        Express app = new Express();
+        Database db = new Database();
+
+        // test
+        app.get("/", (req, res) -> {
+            res.send("Testing!");
+        });
+
+        app.listen(3050);
+        System.out.println("Server started on port 3050.");
+    }
 }
