8e5bfb9f2dbb23ad1768e11a5231f2fab0d7198e
Martina <a.martina.eriksson@hotmail.com>
2020-12-07 14:46:30 +0100

    Added listener and Middleware

--- src/backend/company/NoteServer.java
+++ src/backend/company/NoteServer.java
@@ -3,7 +3,10 @@ package backend.company;
 import backend.company.database.Database;
 import backend.company.notePack.Note;
 import express.Express;
+import express.middleware.Middleware;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class NoteServer {
@@ -18,7 +21,25 @@ public class NoteServer {
             res.send("Testing!");
         });
 
+        // Get notes from database
+        app.get("/rest/notes", (req, res) -> {
+            List<Note> notes = db.getNotes();
+
+            res.json(notes);
+        });
+
+
+
+
+        try {
+            app.use(Middleware.statics(Paths.get("src/frontend.www").toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         app.listen(3050);
         System.out.println("Server started on port 3050.");
+
+
     }
 }
