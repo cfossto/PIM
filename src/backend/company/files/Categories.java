3c464fb89c92917cc4419adc9a82094cd72e1cc1
cfossto <70951673+cfossto@users.noreply.github.com>
2020-12-08 12:37:49 +0100

    Created File and Category classes.

--- src/backend/company/files/Categories.java
+++ src/backend/company/files/Categories.java
@@ -1,4 +1,28 @@
 package backend.company.files;
 
 public class Categories {
+
+    private int id;
+    private String name;
+
+    public Categories(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/backend/company/files/File.java
+++ src/backend/company/files/File.java
@@ -2,10 +2,19 @@ package backend.company.files;
 
 public class File {
 
-    int id;
-    String name;
+    private int id;
+    private int note_id;
+    private int category_id;
+    private String name;
 
     public File (){}
+
+    public File(int id, int note_id, int category_id, String name) {
+        this.id = id;
+        this.note_id = note_id;
+        this.category_id = category_id;
+        this.name = name;
+    }
 
     public File(int id, String name) {
         this.id = id;
@@ -13,5 +22,46 @@ public class File {
     }
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNote_id() {
+        return note_id;
+    }
+
+    public void setNote_id(int note_id) {
+        this.note_id = note_id;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "id=" + id +
+                ", note_id=" + note_id +
+                ", category_id=" + category_id +
+                ", name='" + name + '\'' +
+                '}'+"\n";
+    }
 }
