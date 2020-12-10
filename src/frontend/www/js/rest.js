acb36f95ca447017c6f8d8fae555ac9035202e89
KonnyM <75000328+KonnyM@users.noreply.github.com>
2020-12-10 15:34:36 +0100

    Update rest.js
    Fixed

--- src/frontend/www/js/rest.js
+++ src/frontend/www/js/rest.js
@@ -15,16 +15,11 @@ async function create_note(note) {
 
 async function update_note(note) {
 
-    let edit_note = {
-        id: note.id,
-        title: note.title,
-        list_id: note.list_id,
-        text: note.text
-    }
     let result = await fetch("/rest/notes/id", {
         method: "PUT",
-        body: JSON.stringify(edit_note)
+        body: JSON.stringify(note)
     });
+
 }
 
 async function delete_note(note) {
@@ -38,42 +33,7 @@ async function delete_note(note) {
 async function get_note_lists() {
     let result = await fetch("/rest/lists");
     noteLists = await result.json();
-    async function get_notes() {
-    let result = await fetch("/rest/notes");
-    notes = await result.json();
-    displayNotes();
-    updateNote();
-}
-
-async function create_note(note) {
-
-    let result = await fetch("/rest/notes", {
-        method: "POST",
-        body: JSON.stringify(note)
-    });
-}
-
-async function update_note(note) {
-
-    let result = await fetch("/rest/notes/id", {
-        method: "PUT",
-        body: JSON.stringify(note)
-    });
-
-}
-
-async function delete_note(note) {
-
-    let result = await fetch("/rest/notes/id", {
-        method: "DELETE",
-        body: JSON.stringify(note)
-    });
-}
-
-async function get_note_lists() {
-    let result = await fetch("/rest/lists");
-    lists = await result.json();
-    displayLists();
+    displayLists();	
 }
 
 async function create_note_list(noteList) {
@@ -101,36 +61,6 @@ async function delete_note_list(noteList) {
     });
 }
 
-get_note_lists();
-get_notes();
-update_note();
-}
-
-async function create_note_list(noteList) {
-
-    let result = await fetch("/rest/lists", {
-        method: "POST",
-        body: JSON.stringify(noteList)
-    });
-}
-
-async function update_note_list(noteList) {
-
-    let result = await fetch("/rest/lists/id", {
-        method: "PUT",
-        body: JSON.stringify(noteList)
-    });
-
-}
-
-async function delete_note_list(noteList) {
-
-    let result = await fetch("/rest/lists/id", {
-        method: "DELETE",
-        body: JSON.stringify(noteList)
-    });
-}
-
-get_note_lists();
-get_notes();
-update_note();
+getNoteLists();
+getNotes();
+updateNote();
